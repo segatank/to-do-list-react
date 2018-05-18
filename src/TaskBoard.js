19d12ca71ece55563aab491a9095395101df9468
Maksym Andrieiev <urep@ukr.net>
2018-05-18 12:29:55 +0300

    added styles

--- src/TaskBoard.js
+++ src/TaskBoard.js
@@ -12,26 +12,36 @@ class TaskBoard extends Component {
         "clean the flat"
       ]
     }
+
+    this.handleSortList = this.handleSortList.bind(this);
   }
+
+  handleSortList (val) {
+    console.log(val)
+  }
+
   render() {
     const isItems = this.state.taskArr.length > 0;
 
     return (
-      <section>
-        <h3>What needs to be done:</h3>
-        <input type="text"/>
+      <section className="to-do-list_main-wrapper">
+        <h3 className="to-do-list_header">What needs to be done:</h3>
+        <input type="text" className="to-do-list_input-field" placeholder="add your task"/>
         <section>
         {isItems
           ? <DisplayAllItems listItems={this.state.taskArr} />
           : <div>"Nothing to do."</div>}
         </section>
-        <div>
-          <input type="radio" name="sortTasks" id="all"/>
-            <label htmlFor="all">All</label>
-          <input type="radio" name="sortTasks" id="unfinished"/>
-            <label htmlFor="unfinished">Unfinished</label>
-          <input type="radio" name="sortTasks" id="finished"/>
-            <label htmlFor="finished">Finished</label>
+        <div name="container-footer">
+            <span>Tasks </span>
+            <span>to do: </span>
+            <span>{this.state.taskArr.length} </span>
+            <input type="radio" name="sortTasks" id="all" onChange={this.handleSortList("all")} />
+              <label htmlFor="all">All</label>
+            <input type="radio" name="sortTasks" id="unfinished"/>
+              <label htmlFor="unfinished">Unfinished</label>
+            <input type="radio" name="sortTasks" id="finished"/>
+              <label htmlFor="finished">Finished</label>
         </div>
       </section>
     );
@@ -45,14 +55,14 @@ function DisplayAllItems (props) {
   for (let i=0; i < listArr.length; i++) {
     resultList.push(
       <li key={i.toString()}>
-        <div>
+        <div className="container-list-single-item">
           <input type="checkbox"/>{listArr[i]}
         </div>
       </li>
     );
   }
   return (
-    <div>
+    <div className="container-list-whole">
       <ul>
         {resultList}
       </ul>
