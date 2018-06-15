c3cad56fb1e3a0c0e29c2b9398e35d107b89c850
Maksym Andrieiev <in.hate.i.trust@gmail.com>
2018-06-15 17:08:29 +0300

    added package for generating unique id --shortid--

--- src/AppWrapper/TodoAppWrapper.js
+++ src/AppWrapper/TodoAppWrapper.js
@@ -12,11 +12,11 @@ class TodoAppWrapper extends Component {
 
     this.state = {
       listOfTasks : [
-        {taskName: "walk a dog", taskNumber: 0, status: "unfinished", },
-        {taskName: "feed the cat", taskNumber: 1, status: "finished"},
-        {taskName: "buy some vegies", taskNumber: 2, status: "unfinished"},
-        {taskName: "clean the flat", taskNumber: 3, status: "finished"},
-        {taskName: "wash the window", taskNumber: 4, status: "finished"},
+        {taskName: "walk a dog", taskNumber: generateUniqueId(), status: "unfinished", },
+        {taskName: "feed the cat", taskNumber: generateUniqueId(), status: "finished"},
+        {taskName: "buy some vegies", taskNumber: generateUniqueId(), status: "unfinished"},
+        {taskName: "clean the flat", taskNumber: generateUniqueId(), status: "finished"},
+        {taskName: "wash the window", taskNumber: generateUniqueId(), status: "finished"},
       ],
       filter : "all"
     }
@@ -38,7 +38,7 @@ class TodoAppWrapper extends Component {
 
         const task = {
           taskName: event.target.value,
-          taskNumber: this.state.listOfTasks.length || 0,
+          taskNumber: generateUniqueId(),
           status: "unfinished"
         };
 
@@ -53,7 +53,7 @@ class TodoAppWrapper extends Component {
   handleRemoveTask (event) {
     if (this.state.listOfTasks && this.state.listOfTasks.length > 0) {
       const resultArr = this.state.listOfTasks.filter(
-        arrItem => Number(arrItem.taskNumber) !== Number(event.target.value)
+        arrItem => arrItem.taskNumber !== event.target.value
       );
 
       this.setState({
@@ -69,10 +69,21 @@ class TodoAppWrapper extends Component {
   }
 
   handleModifyTaskStatus (event) {
-    let tmpArr = this.state.listOfTasks;
-    tmpArr[Number(event.target.value)].status === "finished" ?
-      tmpArr[Number(event.target.value)].status = "unfinished" :
-      tmpArr[Number(event.target.value)].status = "finished";
+    const tmpArr = this.state.listOfTasks;
+    let updatedTask;
+    let index;
+
+    for (let i = 0; i < this.state.listOfTasks.length; i++) {
+      if (this.state.listOfTasks[i].taskNumber === event.target.value) {
+        updatedTask = this.state.listOfTasks[i];
+        updatedTask.status === "finished" ?
+          updatedTask.status = "unfinished" :
+          updatedTask.status = "finished";
+        index = i;
+      }
+    }
+    tmpArr[index] = updatedTask;
+
     this.setState({
       listOfTasks: tmpArr
     });
@@ -98,6 +109,12 @@ class TodoAppWrapper extends Component {
       </section>
     );
   }
+}
+
+function generateUniqueId () {
+  let shortid = require('shortid');
+
+  return shortid.generate();
 }
 
 export default TodoAppWrapper;
