f2d588b984b5e5a866457fa7f8936130266d03de
Maksym <in.hate.i.trust@gmail.com>
2018-11-21 16:40:52 +0200

    Updates

---
+++ precache-manifest.a1b23dc9998f1bf6f928d500e6959a67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02cad368e11fe7d376b5",
+    "url": "/to-do-list-react/static/js/runtime~main.02cad368.js"
+  },
+  {
+    "revision": "f824ed30e8b64b0c96e9",
+    "url": "/to-do-list-react/static/js/main.f824ed30.chunk.js"
+  },
+  {
+    "revision": "6413209c0c934bb4e529",
+    "url": "/to-do-list-react/static/js/1.6413209c.chunk.js"
+  },
+  {
+    "revision": "f824ed30e8b64b0c96e9",
+    "url": "/to-do-list-react/static/css/main.e66070a7.chunk.css"
+  },
+  {
+    "revision": "775131eb039210e314c4d3faf4104673",
+    "url": "/to-do-list-react/index.html"
+  }
+];
